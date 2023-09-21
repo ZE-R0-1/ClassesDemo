8a6d07ff47a22badf85a50a8422b4347e5b0a7e1
ZE-R0-1 <jijiij77@naver.com>
2023-09-21 15:45:34 +0900

    Struct vs Class

--- ClassesDemo/Enemy.swift
+++ ClassesDemo/Enemy.swift
@@ -1,7 +1,16 @@
 
-class Enemy {
+struct Enemy {
     var health = 100
     var attackStrength = 10
+    
+    init(health: Int, attackStrength: Int) {
+        self.health = health
+        self.attackStrength = attackStrength
+    }
+    
+    mutating func takeDamage(amount: Int) {
+        health -= amount
+    }
     
     func move() {
         print("Walk forwards.")
--- ClassesDemo/main.swift
+++ ClassesDemo/main.swift
@@ -1,9 +1,20 @@
 
-let skeleton = Enemy()
+var skeleton1 = Enemy(health: 100, attackStrength: 10)
+var skeleton2 = skeleton1
+var skeleton3 = Enemy(health: 100, attackStrength: 10)
 
-let dragon = Dragon()
-dragon.wingSpan = 5
-dragon.attackStrength = 15
-dragon.talk(speech: "My teeth ar swords!")
-dragon.move()
-dragon.attack()
+skeleton1.takeDamage(amount: 10)
+
+print(skeleton1.health)
+print(skeleton2.health)
+print(skeleton3.health)
+
+skeleton1.takeDamage(amount: 10)
+
+print(skeleton1.health)
+print(skeleton2.health)
+
+skeleton2.takeDamage(amount: 10)
+
+print(skeleton1.health)
+print(skeleton2.health)
